20bdacd3dbd4383695c56907522b0d2dd30c73a5
salandarman <bftrade@gmail.com>
2026-06-06 23:34:25 +0300

    added colors and shabbat

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ struct Preferences {
     uptime:String,
     shell:String,
     mem:String,
+    shabbat:String,
 }
 
 fn sh(command:&str) -> String {
@@ -18,7 +19,57 @@ fn sh(command:&str) -> String {
     String::from_utf8_lossy(&take.stdout).trim().to_string()
 }
 
+fn shabbat() -> String {
+    let get_time = sh("timedatectl show | grep -i RTCTimeUSec | awk '{print $3}'");
+    let time_vec: Vec<&str> = get_time.split(':').collect();
+
+    let hours: i32 = time_vec[0].parse().unwrap();
+    let minutes: i32 = time_vec[1].parse().unwrap();
+
+    let is_shabbat = if hours <= 9 && hours >= 6 {
+        if hours == 6 && minutes >= 30 {
+            true
+        }
+        else if hours == 6 && minutes <= 30 {
+            false
+        }
+        else {
+            true
+        }
+    }
+    else {
+        false
+    };
+
+    let min_all = hours*60+minutes;
+
+    let hour_remainder = if 380-min_all > 0 {
+        (380-min_all)/60
+    }
+    else {
+        (380-min_all+1440)/60
+    };
+
+    let minute_remainder = if 380-min_all > 0 {
+        (380-min_all)%60
+    }
+    else {
+        (380-min_all+1440)%60
+    };
+
+    let shabbat_message = if is_shabbat == true {
+        format!("its time to pray shabbat!")
+    }
+    else {
+        format!("{} hours, {} minutes left",hour_remainder,minute_remainder)
+    };
+
+    shabbat_message
+}
+
 fn take_config() -> Preferences {
+    let shabbat_message = shabbat();
+
     Preferences {
         host:sh(r#"echo "$USER@$(cat /etc/hostname)""#),
         os:sh(r#"echo "$(grep '^PRETTY_NAME=' /etc/os-release | cut -d'"' -f2) $(uname -m)""#),
@@ -26,11 +77,17 @@ fn take_config() -> Preferences {
         uptime:sh(r#"uptime -p"#),
         shell:sh(r#"zsh --version | cut -d' ' -f1,2"#),
         mem:sh(r#"free -m | grep Mem | awk '{print $3 "MB / " $2 "MB"}'"#),
+        shabbat:shabbat_message,
     }
 }
 
 fn main() {
     let config = take_config();
+
+    let blue = "\x1b[34m";
+    let reset_color = "\x1b[0m";
+    let space = "  ";
+
     let script = format!(r#"
         /\         {}
        //\\        {}
@@ -39,16 +96,28 @@ fn main() {
    \v/      \v/    Uptime: {}
    /\\      //\    Shell: {}
   //_\\____//_\\   Memory: {}
- '----\\--//----`
-       \\//
-        \/
+ '----\\--//----`  Shabbat: {}
+       \\//        {}
+        \/         {}
         "#,
         config.host,
-        String::from("-").repeat(config.host.chars().count()),
+        "-".repeat(config.host.chars().count()),
         config.os,
         config.kernel,
         config.uptime,
         config.shell,
-        config.mem);
-    println!("{}",script)
+        config.mem,
+        config.shabbat,
+        space,
+        space);
+
+    println!();
+    for line in script.lines() {
+        if line.is_empty() { continue; }
+        else if line.len() > 16 {
+            let (ascii, info) = line.split_at(16);
+            println!("{}{}{}{}",blue,ascii,reset_color,info);
+        }
+    }
+    println!();
 }
